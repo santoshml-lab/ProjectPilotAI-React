285975eec4fcb6353fb5db1fc16796babbbffc48
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-21 10:16:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/dashboard.css";
 import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
