7ba02248987858545a9d8915a1f1504a37e383da
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-21 12:49:24 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,87 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../services/supabase";
+
 export default function Login() {
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  async function handleLogin(e) {
+
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signInWithPassword({
+
+      email,
+
+      password
+
+    });
+
+    if (error) {
+
+      alert(error.message);
+
+      return;
+
+    }
+
+    alert("✅ Login Successful");
+
+    navigate("/dashboard");
+
+  }
+
   return (
-    <div>
-      <h1>🔐 Login Page</h1>
+
+    <div style={{ padding: "50px" }}>
+
+      <h1>🔐 Login</h1>
+
+      <form onSubmit={handleLogin}>
+
+        <input
+
+          type="email"
+
+          placeholder="Email"
+
+          value={email}
+
+          onChange={(e) => setEmail(e.target.value)}
+
+        />
+
+        <br /><br />
+
+        <input
+
+          type="password"
+
+          placeholder="Password"
+
+          value={password}
+
+          onChange={(e) => setPassword(e.target.value)}
+
+        />
+
+        <br /><br />
+
+        <button type="submit">
+
+          Login
+
+        </button>
+
+      </form>
+
     </div>
+
   );
+
 }
