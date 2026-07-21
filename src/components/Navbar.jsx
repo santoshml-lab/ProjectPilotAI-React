95f6cf216cad24f38c59bef3e7ae66abbf6b92ed
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-21 10:07:51 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <header className="navbar">
+      <h2>🚀 ProjectPilot AI</h2>
+
+      <button className="new-btn">
+        + New Project
+      </button>
+    </header>
+  );
+}
