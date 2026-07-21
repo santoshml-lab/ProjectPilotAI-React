073a6de032fedbb491b294ae5be34400145fad59
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-21 13:51:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,38 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import NewProject from "./pages/NewProject";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
+
   return (
+
     <Routes>
+
       <Route path="/" element={<Login />} />
+
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/newproject" element={<NewProject />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/newproject"
+        element={
+          <ProtectedRoute>
+            <NewProject />
+          </ProtectedRoute>
+        }
+      />
+
     </Routes>
+
   );
+
 }
