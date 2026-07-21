d092490fbc8ac9c85cfcc06f86553b021f4b4734
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-21 13:58:11 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
+import LogoutButton from "./LogoutButton";
+
 export default function Navbar() {
+
   return (
+
     <header className="navbar">
+
       <h2>🚀 ProjectPilot AI</h2>
 
-      <button className="new-btn">
-        + New Project
-      </button>
+      <div style={{display:"flex",gap:"15px"}}>
+
+        <button className="new-btn">
+          + New Project
+        </button>
+
+        <LogoutButton/>
+
+      </div>
+
     </header>
+
   );
+
 }
