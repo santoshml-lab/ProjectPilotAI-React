93153217a96640d255236306cc57ab946563ab38
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-21 12:39:55 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -4,7 +4,8 @@ const SUPABASE_URL =
 "https://jpwjsaknniejzpokmire.supabase.co";
 
 const SUPABASE_ANON_KEY =
-"YOUR_SUPABASE_ANON_KEY";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwd2pzYWtubmllanpwb2ttaXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzA2NjgsImV4cCI6MjA5ODMwNjY2OH0.0ur8EhnBmwZGNvBW_ql5pVzOy6OQ9pdDpds_BtYivMg";
+
 
 export const supabase = createClient(
   SUPABASE_URL,
