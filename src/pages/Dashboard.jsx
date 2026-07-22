a0f06f524e83d8140023c14b66b0c025f655f002
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-22 09:39:43 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -210,7 +210,7 @@ export default function Dashboard() {
             <b>Stack:</b> {item.frontend} + {item.backend}
           </p>
 
-          <div
+          
   <div
   style={{
     display: "flex",
